2bfd1518d0a266cdf621859c04cd4525b4f5c3b6
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-24 17:03:39 +0800

    :recycle: Modify Title

--- includes/header.include.php
+++ includes/header.include.php
@@ -2,7 +2,17 @@
     <!-- As a heading -->
     <nav class="navbar navbar-light bg-dark">
         <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1  text-white">PHP FINAL EXAMINATION</span>
+            <div class=d-flex>
+                <div>
+                    <img class="fill-current mr-2" width="70" height="70" src="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg">
+                    <span class="text-white fw-bold">PHP Word Search</span>
+                </div>
+                <div class="d-flex ms-3">
+                    <a href="https://github.com/perilloArt3159/php-word-search.git" class="d-flex align-items-center text-decoration-none text-white fw-bold">
+                        Github Repository 
+                    </a>
+                </div>
+            </div>
         </div>
     </nav>
 </header>
